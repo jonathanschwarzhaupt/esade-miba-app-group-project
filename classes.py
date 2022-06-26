939a7a5b3ece56c7413b64681632eb83ac5a019b
Jonathan <jonathan.schwarzhaupt@gmail.com>
2022-06-26 16:09:13 +0200

    Added functionality for each subclass
    - Function to calculate the area
    - Function to return the equation used to calculate the area

--- classes.py
+++ classes.py
@@ -1,6 +1,8 @@
 # This file contains the classes and functionality for three shapes:
 # Square, Rectangle, and Circle
 
+# Import statements
+import math 
 
 class Shape:
     '''Base class for consequent shapes, specifies color'''
@@ -26,29 +28,59 @@ class Shape:
 
 
 class Square(Shape):
-
+    '''Subclass for square with functionality to calculate area and print its equation'''
     def __init__(self, color: str, length: int) -> None:
         super().__init__(color)
         self.length = length
         self.type = "square"
 
+    def area(self):
+        '''Calculates the area of a square'''
+        area = self.length * self.length
+        return area
+
+    def formula(self):
+        '''Prints the equation to calculate the area of a square'''
+        equation = "A = a*a"
+        return f"The area of a square is calculated by: {equation}"
 
 class Rectangle(Shape):
-    
+    '''Subclass for rectangle with functionality to calculate area and print its equation'''
     def __init__(self, color: str, length: int, width: int) -> None:
         super().__init__(color)
         self.length = length
         self.width = width
         self.type = "rectangle"
+    
+    def area(self):
+        '''Calculates the area of a rectangle'''
+        area = self.length * self.width
+        return area
+
+    def formula(self):
+        '''Prints the equation to calculate the area of a rectangle'''
+        equation = "A = a*a"
+        return f"The area of a square is calculated by: {equation}"
 
 class Circle(Shape):
-
-    def __inti__(self, color: str, radius:int) -> None:
+    '''Subclass for circle with functionality to calculate area and print its equation'''
+    def __init__(self, color: str, radius: int) -> None:
         super().__init__(color)
         self.radius = radius
-        self.diameter = radius*2
+        self.diameter = radius * 2
         self.type = "circle"
+
+    def area(self):
+        '''Calculates the area of a circle'''
+        area = math.pi * self.radius ** 2
+        return area
+    
+    def formula(self):
+        '''Prints the equation to calculate the area of a circle'''
+        equation = "A = pi * radius^2"
+        return f"The area of a circle is calculated by: {equation}"
+
 
 
 if __name__ == "__main__":
-    
+   
