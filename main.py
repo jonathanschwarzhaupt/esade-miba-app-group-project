763c1ee9c63cf769d5ee685a21cb0198b9e4d2fd
vivesgit <carles.vives1@alumni.esade.edu>
2022-07-01 14:29:39 +0200

    Moving InputText

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import pygame
 import button
+import InputBox
 
 pygame.init()
 
 #create game window
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 800
-
-#Extra parameters
-COLOR_INACTIVE = pygame.Color('lightskyblue3')
-COLOR_ACTIVE = pygame.Color('dodgerblue2')
-FONT = pygame.font.Font(None, 32)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("ESADE - Geometric Game")
@@ -19,6 +15,7 @@ pygame.display.set_icon(pygame.image.load("images/logo_icon.png"))
 #game variables
 game_paused = False
 menu_state = "main"
+
 
 #define fonts
 head_font = pygame.font.SysFont("arialblack", 20, italic=False)
@@ -69,53 +66,10 @@ def draw_text(text, font, text_col, x, y):
   img = font.render(text, True, text_col)
   screen.blit(img, (x, y))
 
-#class InputBox
-class InputBox:
-
-    def __init__(self, x, y, w, h, text=''):
-        self.rect = pygame.Rect(x, y, w, h)
-        self.color = COLOR_INACTIVE
-        self.text = text
-        self.txt_surface = FONT.render(text, True, self.color)
-        self.active = False
-
-    def handle_event(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            # If the user clicked on the input_box rect.
-            if self.rect.collidepoint(event.pos):
-                # Toggle the active variable.
-                self.active = not self.active
-            else:
-                self.active = False
-            # Change the current color of the input box.
-            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
-        if event.type == pygame.KEYDOWN:
-            if self.active:
-                if event.key == pygame.K_RETURN:
-                    print(self.text)
-                    self.text = ''
-                elif event.key == pygame.K_BACKSPACE:
-                    self.text = self.text[:-1]
-                else:
-                    self.text += event.unicode
-                # Re-render the text.
-                self.txt_surface = FONT.render(self.text, True, self.color)
-
-    def update(self):
-        # Resize the box if the text is too long.
-        width = max(200, self.txt_surface.get_width()+10)
-        self.rect.w = width
-
-    def draw(self, screen):
-        # Blit the text.
-        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
-        # Blit the rect.
-        pygame.draw.rect(screen, self.color, self.rect, 2)
-
 def sphere_event():
   clock = pygame.time.Clock()
-  input_box1 = InputBox(300, 550, 140, 32)
-  input_box2 = InputBox(300, 650, 140, 32)
+  input_box1 = InputBox.InputBox(300, 550, 140, 32)
+  input_box2 = InputBox.InputBox(300, 650, 140, 32)
   input_boxes = [input_box1, input_box2]
   done = False
 
