0557860ceae3d52a0ce972f166c2dce360b70450
vivesgit <carles.vives1@alumni.esade.edu>
2022-07-01 13:30:38 +0200

    Input box functionality

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ pygame.init()
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 800
 
+#Extra parameters
+COLOR_INACTIVE = pygame.Color('lightskyblue3')
+COLOR_ACTIVE = pygame.Color('dodgerblue2')
+FONT = pygame.font.Font(None, 32)
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("ESADE - Geometric Game")
 pygame.display.set_icon(pygame.image.load("images/logo_icon.png"))
@@ -39,6 +44,8 @@ pentagon_img = pygame.image.load('images/pentagon_img.png').convert_alpha()
 square_img = pygame.image.load('images/square_img.png').convert_alpha()
 triangle_img = pygame.image.load('images/triangle_img.png').convert_alpha()
 
+sphere_big = pygame.image.load('images/sphere_big.png').convert_alpha()
+
 
 #create button instances
 start_button = button.Button(370, 500, start_img, 1)
@@ -62,9 +69,79 @@ def draw_text(text, font, text_col, x, y):
   img = font.render(text, True, text_col)
   screen.blit(img, (x, y))
 
+#class InputBox
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
+
+def sphere_event():
+  clock = pygame.time.Clock()
+  input_box1 = InputBox(300, 550, 140, 32)
+  input_box2 = InputBox(300, 650, 140, 32)
+  input_boxes = [input_box1, input_box2]
+  done = False
+
+  while not done:
+      for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+              done = True
+          for box in input_boxes:
+              box.handle_event(event)
+
+      for box in input_boxes:
+          box.update()
+
+      screen.fill((255, 255, 255))
+      for box in input_boxes:
+          box.draw(screen)
+      screen.blit(sphere_big, (300, 100))    
+
+      pygame.display.flip()
+      clock.tick(30)
+
+
 #game loop
 run = True
 menu_state = 'init'
+
 while run:
 
   screen.fill((255, 255, 255))
@@ -80,6 +157,7 @@ while run:
     screen.fill((255, 255, 255))
     draw_text("Select the geometric shape of the figure", head_font, TEXT_COL, 250, 200)
     if sphere_button.draw(screen):
+      sphere_event()
       print("Rectangle function")
     if cone_button.draw(screen):
       print("Cone function")
