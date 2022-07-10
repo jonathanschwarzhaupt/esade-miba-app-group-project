e3c3e4df05b373e85c79026c7448386d01eae04d
Alissa Clémence <89780731+AlissaClemence21@users.noreply.github.com>
2022-07-10 20:56:48 +0200

    Update game.py

--- Enhancement- Alissa/game.py
+++ Enhancement- Alissa/game.py
@@ -1,18 +1,19 @@
 import pygame
 from menu import *
 
-
+"""This is the main game class. It handles the game loop and the main menu."""
 class Game():
     def __init__(self):
         pygame.init()
+        """Initialize the game."""
         self.running, self.playing = True, False
         self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
         self.DISPLAY_W, self.DISPLAY_H = 480, 270
         self.display = pygame.Surface((self.DISPLAY_W,self.DISPLAY_H))
         self.window = pygame.display.set_mode(((self.DISPLAY_W,self.DISPLAY_H)))
         self.font_name = '8-BIT WONDER.TTF'
-        #self.font_name = pygame.font.get_default_font()
         self.BLACK, self.WHITE = (0, 0, 0), (255, 255, 255)
+        """"Creating the main menus"""
         self.main_menu = MainMenu(self)
         self.shapes_menu = ShapesMenu(self)
         self.options = OptionsMenu(self)
@@ -22,8 +23,10 @@ class Game():
         self.rectangle = Rectangle(self)
         self.triangle = Triangle(self)
         self.circle = Circle(self)
-        self.input_box = InputBox(self)
+        #self.input_box = InputBox(self)
         self.curr_menu = self.main_menu
+
+    """"This is the main function. It runs the game loop and the main menu."""
 
     def game_loop(self):
         while self.playing:
@@ -31,6 +34,7 @@ class Game():
             if self.START_KEY:
                 self.playing= False
             self.display.fill(self.BLACK)
+            """Drawing the current menu"""
             self.draw_text('Explanations', 20, self.DISPLAY_W/2, self.DISPLAY_H/2)
             self.draw_text("You can choose a shape and input",12, self.DISPLAY_W/2, self.DISPLAY_H/2+20)
             self.draw_text("its dimensions to return its area",12, self.DISPLAY_W/2, self.DISPLAY_H/2+30)
@@ -39,7 +43,7 @@ class Game():
             self.reset_keys()
 
 
-
+    """"This is the main function. It runs the game loop and the main menu."""
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -54,7 +58,7 @@ class Game():
                     self.DOWN_KEY = True
                 if event.key == pygame.K_UP:
                     self.UP_KEY = True
-
+    """"Reseting the keys."""
     def reset_keys(self):
         self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
 
